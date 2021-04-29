fbcf999e3e445801e199ef2a38d63bb3a5a21ae9
vananhthomeo <62092148+vananhthomeo@users.noreply.github.com>
2021-04-29 22:50:31 +0700

    van anh update login page

--- Myaccount/myaccount.js
+++ Myaccount/myaccount.js
@@ -1,20 +1,37 @@
+console.log(sessionStorage.getItem("isLog"));
+console.log(document.getElementById('info-page'))
+if (sessionStorage.getItem("isLog")){
+        document.getElementById('info-page').style.display = 'block';
+        document.getElementById('login-page').style.display = 'none';
+        
+    }else{
+        document.getElementById('info-page').style.display = 'none';
+        document.getElementById('login-page').style.display = 'block';
+    }
 
 function validateForm() {
     email = document.getElementById('email-login').value;
     password =  document.getElementById('password').value
-    if ( email== "") {
+    if ( email=== "") {
         setErrorFor(document.getElementById('email-login'), 'Email cannot be blank');
     }else{
         setSuccessFor(document.getElementById('email-login'));
     }
-    if (password== "") {
+    if (password=== "") {
         setErrorFor(document.getElementById('password'), 'Password cannot be blank');
-    } else if (password =="password") {
-        setSuccesLogin();
+    } else if (password ==="1") {
+        storeInfo();
+        document.getElementById('info-page').style.display = 'block';
+        document.getElementById('login-page').style.display = 'none';
     }
     else{
         setErrorFor(document.getElementById('password'), 'Password is incorrect');;
     }
+}
+function storeInfo(){
+    sessionStorage.setItem('email', document.getElementById('email-login').value);
+    
+    sessionStorage.setItem('isLog', true);
 }
 function setErrorFor(input, message) {
 	const formControl = input.parentElement;
@@ -26,9 +43,10 @@ function setSuccessFor(input) {
 	const formControl = input.parentElement;
 	formControl.className = 'form-control success';
 }
+
 /* fucntion to display none */
-function setSuccesLogin() {
-  document.getElementsByClassName("container-login")[0].style.display ="none";
-  document.getElementsByClassName("information-account")[0].style.display ="block";
-}
+// document.getElementById('info-page').style.display =sessionStorage.getItem('display');
+// document.getElementById('login-page').style.display =sessionStorage.getItem('display-none')
+
+// document.getElementById("email").value =sessionStorage.getItem('email');
 
